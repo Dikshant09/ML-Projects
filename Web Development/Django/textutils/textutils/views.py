f13b5696704f4c5bb1c08f6142ea1852ccdea511
Dikshant <Dikshant1535.be20@chitkara.edu.in>
2022-02-02 18:04:30 +0530

    Fixed some bugs

--- Web Development/Django/textutils/textutils/views.py
+++ Web Development/Django/textutils/textutils/views.py
@@ -1,11 +1,9 @@
-# I have created this file - Harry
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'index.html')
-
 
 def analyze(request):
     #Get the text
